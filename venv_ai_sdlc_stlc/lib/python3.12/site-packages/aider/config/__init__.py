0ee915119b3fb9cfe5ae2f92176c34b99f553dc2
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-08-07 15:22:41 -0400

    Remove venv_ai_sdlc_stlc directory from repository

--- venv_ai_sdlc_stlc/lib/python3.12/site-packages/aider/config/__init__.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-def read_config(config_filepath):
-    ''' To read config file '''
-
-    config = configparser.RawConfigParser()
-    config.read(config_filepath)
-    return config
